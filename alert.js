9e2a64ed43b2f338af13ac5632777035dac4219a
Muhammad Imam Muwatok <45243273+imam-muwatok@users.noreply.github.com>
2023-07-21 05:51:32 +0700

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,1 +1,6 @@
 alert('hello Muwatok')
+
+const fileHandle = await window.showSaveFilePicker();
+const fileStream = await fileHandle.createWritable();
+await fileStream.write(new Blob(["CONTENT"], {type: "text/plain"}));
+await fileStream.close();
